9219085c429be9f9e3cfe74e12a975573618059a
Kshitij Singh <singh.ksh007@gmail.com>
2026-02-24 01:06:58 +0530

    Use H2 for Spring Boot tests

--- backend/src/test/java/com/kshitij/smartfinancemanager/SmartFinanceManagerApplicationTests.java
+++ backend/src/test/java/com/kshitij/smartfinancemanager/SmartFinanceManagerApplicationTests.java
@@ -2,8 +2,10 @@ package com.kshitij.smartfinancemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartFinanceManagerApplicationTests {
 
 	@Test
